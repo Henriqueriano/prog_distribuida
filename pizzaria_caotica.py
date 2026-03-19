7672da5170e1493331fcc1c421e4a7794137694c
Daniel Henrique <daniel.neves0021@gmail.com>
2026-03-18 21:03:20 -0300

    fix: local merge fixed

---
+++ pizzaria_caotica.py
@@ -0,0 +1,9 @@
+import queue
+import threading
+import time
+
+class Myth(threading.Thread):
+    def __init__(self, name):
+        threading.Thread.__init__(self)
+        
+if __name__ == '__main__':
